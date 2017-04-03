ccb09f31df55036abfeb67140e7ea0e407d90e22
Heisenberg Kim <heisenberg.kim@gmail.com>
2017-04-03 00:14:57 -0700

    3.py from develop

---
+++ 3.py
@@ -0,0 +1,16 @@
+The result:
+
+Current date and time using str method of datetime object:
+2013-02-17 16:02:49.338517
+
+Current date and time using instance attributes:
+Current year: 2013
+Current month: 2
+Current day: 17
+Current hour: 16
+Current minute: 2
+Current second: 49
+Current microsecond: 338517
+
+Current date and time using strftime:
+2013-02-17 16:02
